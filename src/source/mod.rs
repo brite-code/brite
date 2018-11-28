1e57131ad126a11566549dd84a976d3d710acd8c
Caleb Meredith <calebmeredith8@gmail.com>
2018-11-28 15:53:36 -0800

    Add a root parse function.

--- src/source/mod.rs
+++ src/source/mod.rs
@@ -9,4 +9,16 @@ mod parser;
 mod position;
 mod token;
 
+pub use self::ast::*;
+pub use self::document::Document;
 pub use self::position::{Position, Range};
+pub use self::token::*;
+
+use crate::diagnostics::Diagnostic;
+use std::rc::Rc;
+
+/// Parses a Brite source document into an Abstract Syntax Tree (AST). We can handle any source
+/// text thrown at this function. Only well formatted code will be useful, though.
+pub fn parse(document: &Document) -> (Vec<Rc<Diagnostic>>, Module) {
+    unimplemented!()
+}
