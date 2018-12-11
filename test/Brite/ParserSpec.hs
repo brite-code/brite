f00bfc389a3fc595e78ab6e3c83e6193b943e71b
Caleb Meredith <calebmeredith8@gmail.com>
2018-12-11 14:52:57 -0800

    Add tests for binding statement.

--- test/Brite/ParserSpec.hs
+++ test/Brite/ParserSpec.hs
@@ -25,8 +25,191 @@ testParse input expected =
   in
     actual `shouldBe` expected
 
-spec =
-  it "parses a binding statement" $
-    testParse
-      "let x = y"
-      "(bind (var 0:4-0:5 `x`) (var 0:8-0:9 `y`))\n"
+spec = mapM_ (\(input, expected) -> it (T.unpack input) $ testParse input expected)
+  [ ( "let x = y"
+    , "(bind (var 0:4-0:5 `x`) (var 0:8-0:9 `y`))\n"
+    )
+  , ( "let"
+    , "(0:3-0:3) We wanted a variable name but the file ended.\n\
+      \(0:3-0:3) We wanted `=` but the file ended.\n\
+      \(0:3-0:3) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind err err))\n"
+    )
+  , ( "let x"
+    , "(0:5-0:5) We wanted `=` but the file ended.\n\
+      \(0:5-0:5) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `x`) err))\n"
+    )
+  , ( "let ="
+    , "(0:4-0:5) We wanted a variable name but we found `=`.\n\
+      \(0:5-0:5) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(bind err err)\n"
+    )
+  , ( "let y"
+    , "(0:5-0:5) We wanted `=` but the file ended.\n\
+      \(0:5-0:5) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `y`) err))\n"
+    )
+  , ( "let x ="
+    , "(0:7-0:7) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(bind (var 0:4-0:5 `x`) err)\n"
+    )
+  , ( "let = y"
+    , "(0:4-0:5) We wanted a variable name but we found `=`.\n\
+      \\n\
+      \(bind err (var 0:6-0:7 `y`))\n"
+    )
+  , ( "let x y"
+    , "(0:6-0:7) We wanted `=` but we found a variable name.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `x`) (var 0:6-0:7 `y`)))\n"
+    )
+  , ( "let x = y"
+    , "(bind (var 0:4-0:5 `x`) (var 0:8-0:9 `y`))\n"
+    )
+  , ( "😈 let x = y"
+    , "(0:0-0:2) We wanted `let` but we found `😈`.\n\
+      \\n\
+      \(err (bind (var 0:7-0:8 `x`) (var 0:11-0:12 `y`)))\n"
+    )
+  , ( "let 😈 x = y"
+    , "(0:4-0:6) We wanted a variable name but we found `😈`.\n\
+      \\n\
+      \(bind (err (var 0:7-0:8 `x`)) (var 0:11-0:12 `y`))\n"
+    )
+  , ( "let x 😈 = y"
+    , "(0:6-0:8) We wanted `=` but we found `😈`.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `x`) (var 0:11-0:12 `y`)))\n"
+    )
+  , ( "let x = 😈 y"
+    , "(0:8-0:10) We wanted a variable name but we found `😈`.\n\
+      \\n\
+      \(bind (var 0:4-0:5 `x`) (err (var 0:11-0:12 `y`)))\n"
+    )
+  , ( "let x = y 😈"
+    , "(bind (var 0:4-0:5 `x`) (var 0:8-0:9 `y`))\n"
+    )
+  , ( ") let x = y"
+    , "(0:0-0:1) We wanted `let` but we found `)`.\n\
+      \\n\
+      \(err (bind (var 0:6-0:7 `x`) (var 0:10-0:11 `y`)))\n"
+    )
+  , ( "let ) x = y"
+    , "(0:4-0:5) We wanted a variable name but we found `)`.\n\
+      \\n\
+      \(bind (err (var 0:6-0:7 `x`)) (var 0:10-0:11 `y`))\n"
+    )
+  , ( "let x ) = y"
+    , "(0:6-0:7) We wanted `=` but we found `)`.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `x`) (var 0:10-0:11 `y`)))\n"
+    )
+  , ( "let x = ) y"
+    , "(0:8-0:9) We wanted a variable name but we found `)`.\n\
+      \\n\
+      \(bind (var 0:4-0:5 `x`) (err (var 0:10-0:11 `y`)))\n"
+    )
+  , ( "let x = y )"
+    , "(bind (var 0:4-0:5 `x`) (var 0:8-0:9 `y`))\n"
+    )
+  , ( "let 😈 = y"
+    , "(0:4-0:6) We wanted a variable name but we found `😈`.\n\
+      \\n\
+      \(bind err (var 0:9-0:10 `y`))\n"
+    )
+  , ( "let x 😈 y"
+    , "(0:6-0:8) We wanted `=` but we found `😈`.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `x`) (var 0:9-0:10 `y`)))\n"
+    )
+  , ( "let x = 😈"
+    , "(0:8-0:10) We wanted a variable name but we found `😈`.\n\
+      \\n\
+      \(bind (var 0:4-0:5 `x`) err)\n"
+    )
+  , ( "let 😈 y"
+    , "(0:4-0:6) We wanted a variable name but we found `😈`.\n\
+      \(0:8-0:8) We wanted `=` but the file ended.\n\
+      \(0:8-0:8) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind (err (var 0:7-0:8 `y`)) err))\n"
+    )
+  , ( "let 😈 ="
+    , "(0:4-0:6) We wanted a variable name but we found `😈`.\n\
+      \(0:8-0:8) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(bind err err)\n"
+    )
+  , ( "let x 😈"
+    , "(0:6-0:8) We wanted `=` but we found `😈`.\n\
+      \(0:8-0:8) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `x`) err))\n"
+    )
+  , ( "let = 😈"
+    , "(0:4-0:5) We wanted a variable name but we found `=`.\n\
+      \(0:6-0:8) We wanted a variable name but we found `😈`.\n\
+      \\n\
+      \(bind err err)\n"
+    )
+  , ( "let 😈"
+    , "(0:4-0:6) We wanted a variable name but we found `😈`.\n\
+      \(0:6-0:6) We wanted `=` but the file ended.\n\
+      \(0:6-0:6) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind err err))\n"
+    )
+  , ( "let ) = y"
+    , "(0:4-0:5) We wanted a variable name but we found `)`.\n\
+      \\n\
+      \(bind err (var 0:8-0:9 `y`))\n"
+    )
+  , ( "let x ) y"
+    , "(0:6-0:7) We wanted `=` but we found `)`.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `x`) (var 0:8-0:9 `y`)))\n"
+    )
+  , ( "let x = )"
+    , "(0:8-0:9) We wanted a variable name but we found `)`.\n\
+      \\n\
+      \(bind (var 0:4-0:5 `x`) err)\n"
+    )
+  , ( "let ) y"
+    , "(0:4-0:5) We wanted a variable name but we found `)`.\n\
+      \(0:7-0:7) We wanted `=` but the file ended.\n\
+      \(0:7-0:7) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind (err (var 0:6-0:7 `y`)) err))\n"
+    )
+  , ( "let ) ="
+    , "(0:4-0:5) We wanted a variable name but we found `)`.\n\
+      \(0:7-0:7) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(bind err err)\n"
+    )
+  , ( "let x )"
+    , "(0:6-0:7) We wanted `=` but we found `)`.\n\
+      \(0:7-0:7) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind (var 0:4-0:5 `x`) err))\n"
+    )
+  , ( "let = )"
+    , "(0:4-0:5) We wanted a variable name but we found `=`.\n\
+      \(0:6-0:7) We wanted a variable name but we found `)`.\n\
+      \\n\
+      \(bind err err)\n"
+    )
+  , ( "let )"
+    , "(0:4-0:5) We wanted a variable name but we found `)`.\n\
+      \(0:5-0:5) We wanted `=` but the file ended.\n\
+      \(0:5-0:5) We wanted a variable name but the file ended.\n\
+      \\n\
+      \(err (bind err err))\n"
+    )
+  ]
