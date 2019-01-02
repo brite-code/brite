d46ddad677ea25973f522164c5b606abe9797cc1
Caleb Meredith <calebmeredith8@gmail.com>
2019-01-02 14:32:38 -0800

    Consolidate layout parameters into state.

--- src/Brite/Syntax/PrinterFramework.hs
+++ src/Brite/Syntax/PrinterFramework.hs
@@ -103,46 +103,58 @@ shamefullyUngroup x = x
 -- The current mode in which a document is being printed.
 data Mode = Flat | Break
 
+data LayoutState = LayoutState
+  -- The current character position measured in UTF-16 encoded code units as specified
+  -- by the LSP.
+  { layoutCharacter :: Int
+  -- Line suffix documents. This list is reverse ordered.
+  , layoutLineSuffix :: [Document]
+  }
+
 -- Prints the document at the specified maximum width.
 printDocument :: Int -> Document -> B.Builder
-printDocument maxWidth rootDocument = layout 0 [(Break, 0, rootDocument)] []
+printDocument maxWidth rootDocument = layout initialState [(Break, 0, rootDocument)]
   where
+    initialState = LayoutState
+      { layoutCharacter = 0
+      , layoutLineSuffix = []
+      }
+
     -- Responsible for laying out a document. Takes as parameters:
     --
-    -- * The current character position measured in UTF-16 encoded code units as specified
-    --   by the LSP.
+    -- * State for our layout process.
     -- * An execution stack of documents. We build our document by processing every item on the
     --   stack. Each stack item contains the mode in which we print the document, the current level
     --   of indentation, and the document to be printed.
-    -- * Line suffix documents. This list is reverse ordered.
-    layout :: Int -> [(Mode, Int, Document)] -> [Document] -> B.Builder
+    layout :: LayoutState -> [(Mode, Int, Document)] -> B.Builder
 
     -- We are done processing our stack!
-    layout _ [] [] = mempty
+    layout (LayoutState { layoutLineSuffix = [] }) [] = mempty
 
     -- If there are some line suffix documents remaining add them to our stack and process them.
-    layout k [] suffix = layout k (map ((,,) Break 0) (reverse suffix)) []
+    layout s [] =
+      layout (s { layoutLineSuffix = [] }) (map ((,,) Break 0) (reverse (layoutLineSuffix s)))
 
     -- Skip empty documents in the stack.
-    layout k ((_, _, Empty) : stack) suffix = layout k stack suffix
+    layout s ((_, _, Empty) : stack) = layout s stack
 
     -- Add both documents of a concatenation to the stack.
-    layout k ((m, i, Concat x y) : stack) suffix = layout k ((m, i, x) : (m, i, y) : stack) suffix
+    layout s ((m, i, Concat x y) : stack) = layout s ((m, i, x) : (m, i, y) : stack)
 
     -- Make a choice depending on the mode. `Flat` chooses the left and `Break` chooses the right.
-    layout k ((Flat, i, Choice x _) : stack) suffix = layout k ((Flat, i, x) : stack) suffix
-    layout k ((Break, i, Choice _ y) : stack) suffix = layout k ((Break, i, y) : stack) suffix
+    layout s ((Flat, i, Choice x _) : stack) = layout s ((Flat, i, x) : stack)
+    layout s ((Break, i, Choice _ y) : stack) = layout s ((Break, i, y) : stack)
 
     -- Flat groups continue to flatten their document. It is break groups which are interesting.
     -- Break mode groups attempt to flatten their document.
-    layout k ((Flat, i, Group x) : stack) suffix = layout k ((Flat, i, x) : stack) suffix
+    layout s ((Flat, i, Group x) : stack) = layout s ((Flat, i, x) : stack)
 
     -- A break mode group attempts to flatten its document using the `tryLayout` function to see if
     -- it will fit on one line. If it does not then we layout the group using our break mode.
-    layout k ((Break, i, Group x) : stack) suffix =
-      case tryLayout k ((Flat, i, x) : stack) suffix of
+    layout s ((Break, i, Group x) : stack) =
+      case tryLayout s ((Flat, i, x) : stack) of
         Just t -> t
-        Nothing -> layout k ((Break, i, x) : stack) suffix
+        Nothing -> layout s ((Break, i, x) : stack)
 
     -- Add text to the document. If the text is a single line then we add the UTF-16 encoded code
     -- point length to the position to get our new position. If the text is multiple lines we set
@@ -150,13 +162,15 @@ printDocument maxWidth rootDocument = layout 0 [(Break, 0, rootDocument)] []
     --
     -- Remember that we use UTF-16 to measure length because that is the encoding specified for
     -- character widths by the Language Server Protocol (LSP) specification.
-    layout k0 ((_, _, Text t0) : stack) suffix = B.fromText t0 <> loop k0 t0
+    layout s ((_, _, Text t0) : stack) =
+      let k = loop (layoutCharacter s) t0 in
+        B.fromText t0 <> layout (s { layoutCharacter = k }) stack
       where
         -- Measure the length of our text until we either reach the text’s end or a new line. If we
         -- reach a new line then use `loopBack` to measure the length of the last line.
         loop k t1 =
           case T.uncons t1 of
-            Nothing -> layout k stack suffix
+            Nothing -> k
             Just ('\n', t2) -> loopBack 0 t2
             Just ('\r', t2) -> loopBack 0 t2
             Just (c, t2) -> loop (k + utf16Length c) t2
@@ -165,51 +179,53 @@ printDocument maxWidth rootDocument = layout 0 [(Break, 0, rootDocument)] []
         -- length of the last line.
         loopBack k t1 =
           case T.unsnoc t1 of
-            Nothing -> layout k stack suffix
-            Just (_, '\n') -> layout k stack suffix
-            Just (_, '\r') -> layout k stack suffix
+            Nothing -> k
+            Just (_, '\n') -> k
+            Just (_, '\r') -> k
             Just (t2, c) -> loopBack (k + utf16Length c) t2
 
     -- Add some indentation.
-    layout k ((m, i, Indent x) : stack) suffix = layout k ((m, i + 2, x) : stack) suffix
+    layout s ((m, i, Indent x) : stack) = layout s ((m, i + 2, x) : stack)
 
     -- Add a new line at the current level of indentation. If there are some line suffix documents
     -- then add them to the execution stack and process them.
-    layout _ ((_, i, Line) : stack) [] =
-      B.singleton '\n' <> B.fromText (T.replicate i " ") <> layout i stack []
-    layout k stack@((m, i, Line) : _) suffix =
-      layout k (foldl (flip (:)) stack (map ((,,) m i) suffix)) []
+    layout (LayoutState { layoutLineSuffix = [] }) ((_, i, Line) : stack) =
+      let s = LayoutState { layoutCharacter = i, layoutLineSuffix = [] } in
+        B.singleton '\n' <> B.fromText (T.replicate i " ") <> layout s stack
+    layout s stack@((m, i, Line) : _) =
+      layout (s { layoutLineSuffix = [] }) $
+        foldl (flip (:)) stack (map ((,,) m i) (layoutLineSuffix s))
 
     -- Add line suffix documents to the suffix stack.
-    layout k ((_, _, LineSuffix x) : stack) suffix = layout k stack (x : suffix)
+    layout s ((_, _, LineSuffix x) : stack) =
+      layout (s { layoutLineSuffix = x : layoutLineSuffix s }) stack
 
     -- Attempts to layout a flat mode document.
     --
     -- * Returns `Nothing` if `k` exceeds `maxWidth`.
     -- * Returns `Nothing` if we find a new line in a flat mode document.
     -- * Returns `Just` if the first line fits in `maxWidth`.
-    tryLayout :: Int -> [(Mode, Int, Document)] -> [Document] -> Maybe B.Builder
+    tryLayout :: LayoutState -> [(Mode, Int, Document)] -> Maybe B.Builder
 
     -- If our line has exceeded `maxWidth` return `Nothing`.
-    tryLayout k _ _ | k > maxWidth = Nothing
+    tryLayout s _ | layoutCharacter s > maxWidth = Nothing
 
     -- If we have reached the end of the stack we’re ok!
-    tryLayout k [] suffix = Just (layout k [] suffix)
+    tryLayout s [] = Just (layout s [])
 
     -- Skip empty documents in the stack.
-    tryLayout k ((_, _, Empty) : stack) suffix = tryLayout k stack suffix
+    tryLayout s ((_, _, Empty) : stack) = tryLayout s stack
 
     -- Add both documents of a concatenation to the stack.
-    tryLayout k ((m, i, Concat x y) : stack) suffix =
-      tryLayout k ((m, i, x) : (m, i, y) : stack) suffix
+    tryLayout s ((m, i, Concat x y) : stack) = tryLayout s ((m, i, x) : (m, i, y) : stack)
 
     -- Make a choice depending on the mode. `Flat` chooses the left and `Break` chooses the right.
-    tryLayout k ((Flat, i, Choice x _) : stack) suffix = tryLayout k ((Flat, i, x) : stack) suffix
-    tryLayout k ((Break, i, Choice _ y) : stack) suffix = tryLayout k ((Break, i, y) : stack) suffix
+    tryLayout s ((Flat, i, Choice x _) : stack) = tryLayout s ((Flat, i, x) : stack)
+    tryLayout s ((Break, i, Choice _ y) : stack) = tryLayout s ((Break, i, y) : stack)
 
     -- Attempt to layout a group in the current specified mode. We don’t attempt to flatten break
     -- mode documents. Instead we lay them out as-is.
-    tryLayout k ((m, i, Group x) : stack) suffix = tryLayout k ((m, i, x) : stack) suffix
+    tryLayout s ((m, i, Group x) : stack) = tryLayout s ((m, i, x) : stack)
 
     -- Add text to the document. If the document is a single-line we make sure it does not exceed
     -- `maxWidth`. If it does then we return `Nothing`. If the document is multi-line and we are in
@@ -219,7 +235,8 @@ printDocument maxWidth rootDocument = layout 0 [(Break, 0, rootDocument)] []
     --
     -- Remember that we use UTF-16 to measure length because that is the encoding specified for
     -- character widths by the Language Server Protocol (LSP) specification.
-    tryLayout k0 ((m, _, Text t0) : stack) suffix = (B.fromText t0 <>) <$> loop k0 t0
+    tryLayout s ((m, _, Text t0) : stack) =
+      (B.fromText t0 <>) <$> loop (layoutCharacter s) t0
       where
         -- Measure the length of our text until we either reach the text’s end or a new line. If we
         -- reach a new line and we are in break mode then use `loopBack` to measure the length of
@@ -227,7 +244,7 @@ printDocument maxWidth rootDocument = layout 0 [(Break, 0, rootDocument)] []
         loop k t1 =
           case T.uncons t1 of
             _ | k > maxWidth -> Nothing -- Optimization: Stop iterating if we’ve surpassed `maxWidth`.
-            Nothing -> tryLayout k stack suffix
+            Nothing -> tryLayout (s { layoutCharacter = k }) stack
             Just ('\n', t2) -> case m of { Flat -> Nothing; Break -> loopBack 0 t2 }
             Just ('\r', t2) -> case m of { Flat -> Nothing; Break -> loopBack 0 t2 }
             Just (c, t2) -> loop (k + utf16Length c) t2
@@ -236,20 +253,20 @@ printDocument maxWidth rootDocument = layout 0 [(Break, 0, rootDocument)] []
         -- length of the last line.
         loopBack k t1 =
           case T.unsnoc t1 of
-            Nothing -> Just (layout k stack suffix)
-            Just (_, '\n') -> Just (layout k stack suffix)
-            Just (_, '\r') -> Just (layout k stack suffix)
+            Nothing -> Just (layout (s { layoutCharacter = k }) stack)
+            Just (_, '\n') -> Just (layout (s { layoutCharacter = k }) stack)
+            Just (_, '\r') -> Just (layout (s { layoutCharacter = k }) stack)
             Just (t2, c) -> loopBack (k + utf16Length c) t2
 
     -- Add some indentation.
-    tryLayout k ((m, i, Indent x) : stack) suffix = tryLayout k ((m, i + 2, x) : stack) suffix
+    tryLayout s ((m, i, Indent x) : stack) = tryLayout s ((m, i + 2, x) : stack)
 
     -- If we are in flat mode and we find a new line then fail `tryLayout` by returning `Nothing`.
     -- We may not have new lines in flat mode. If we are in break mode then add a new line and
     -- return `Just` since our line that we are attempting to layout did not exceed `maxWidth`!
-    tryLayout _ ((Flat, _, Line) : _) _ = Nothing
-    tryLayout k stack@((Break, _, Line) : _) suffix = Just (layout k stack suffix)
+    tryLayout _ ((Flat, _, Line) : _) = Nothing
+    tryLayout s stack@((Break, _, Line) : _) = Just (layout s stack)
 
     -- Add line suffix documents to the suffix stack.
-    tryLayout k ((_, _, LineSuffix x) : stack) suffix = tryLayout k stack (x : suffix)
-
+    tryLayout s ((_, _, LineSuffix x) : stack) =
+      tryLayout (s { layoutLineSuffix = x : layoutLineSuffix s }) stack
