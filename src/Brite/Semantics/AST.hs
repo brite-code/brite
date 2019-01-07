619085f188635e4e87f12ed65d561ce9780aa385
Caleb Meredith <calebmeredith8@gmail.com>
2019-01-07 15:14:36 -0800

    Start writing AST to CST conversion.

--- src/Brite/Semantics/AST.hs
+++ src/Brite/Semantics/AST.hs
@@ -71,6 +71,8 @@ data StatementNode
 
   -- Marks the position of some error in the AST. We may or may not have been able to recover from
   -- the error. If we recovered then the AST node will be `Just` otherwise it will be `Nothing`.
+  --
+  -- We will panic at runtime if someone attempts to execute this node.
   | ErrorStatement Diagnostic (Maybe Statement)
 
 -- NOTE: Eventually we will also add a `TypeDeclaration`.
@@ -162,6 +164,8 @@ data ExpressionNode
 
   -- Marks the position of some error in the AST. We may or may not have been able to recover from
   -- the error. If we recovered then the AST node will be `Just` otherwise it will be `Nothing`.
+  --
+  -- We will panic at runtime if someone attempts to execute this node.
   | ErrorExpression Diagnostic (Maybe Expression)
 
 -- `p: E`
@@ -236,6 +240,8 @@ data PatternNode
 
   -- Marks the position of some error in the AST. We may or may not have been able to recover from
   -- the error. If we recovered then the AST node will be `Just` otherwise it will be `Nothing`.
+  --
+  -- We will panic at runtime if someone attempts to execute this node.
   | ErrorPattern Diagnostic (Maybe Pattern)
 
 -- `p: P`
@@ -274,6 +280,8 @@ data TypeNode
 
   -- Marks the position of some error in the AST. We may or may not have been able to recover from
   -- the error. If we recovered then the AST node will be `Just` otherwise it will be `Nothing`.
+  --
+  -- We will panic at runtime if someone attempts to execute this node.
   | ErrorType Diagnostic (Maybe Type)
 
 -- `p: T`
@@ -281,3 +289,53 @@ data ObjectTypeProperty = ObjectTypeProperty Name (Maybe Type)
 
 -- `x: T`
 data Quantifier = Quantifier Name (Maybe (QuantifierBoundKind, Type))
+
+-- A monad for converting CST nodes to AST nodes in the same form as the CST `Recover` data type. If
+-- we recover multiple times then we will use the first error.
+data Recover a
+  = Ok a
+  | Recover Diagnostic a
+  | Fatal Diagnostic
+
+instance Functor Recover where
+  fmap f (Ok a) = Ok (f a)
+  fmap f (Recover e a) = Recover e (f a)
+  fmap _ (Fatal e) = Fatal e
+
+instance Applicative Recover where
+  pure a = Ok a
+
+  Fatal e <*> _ = Fatal e
+  _ <*> Fatal e = Fatal e
+  Recover e f <*> Recover _ a = Recover e (f a)
+  Recover e f <*> Ok a = Recover e (f a)
+  Ok f <*> Recover e a = Recover e (f a)
+  Ok f <*> Ok a = Ok (f a)
+
+instance Monad Recover where
+  Ok a >>= f = f a
+  Fatal e >>= _ = Fatal e
+  Recover e a >>= f =
+    case f a of
+      Fatal e2 -> Fatal e2
+      Recover _ b -> Recover e b
+      Ok b -> Recover e b
+
+-- Converts a CST `Recover` into an AST `Recover`.
+recover :: CST.Recover a -> Recover a
+recover (CST.Fatal _ e) = Fatal e
+recover (CST.Recover _ e a) = Recover e a
+recover (CST.Ok a) = Ok a
+
+-- Runs the `Recover` monad and takes an error constructor for the node. The error constructor is
+-- called if there is a fatal error or an error recovery.
+runRecover :: (Diagnostic -> Maybe a -> a) -> Recover a -> a
+runRecover f (Fatal e) = f e Nothing
+runRecover f (Recover e a) = f e (Just a)
+runRecover _ (Ok a) = a
+
+-- -- Converts a CST expression into an AST expression.
+-- expressionFromCST :: CST.Expression -> Expression
+-- expressionFromCST x0 = runRecover build $ case x0 of
+--   CST.ConstantExpression (CST.BooleanConstant b t) ->
+--     return (Expression (tokenRange t) (ConstantExpression (BooleanConstant b)))
