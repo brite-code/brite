311eb32ae794327829785d9aea97ef82930d45a3
Caleb Meredith <calebmeredith8@gmail.com>
2019-01-16 14:22:45 -0800

    Add a bunch of stuff to the CLI module.

--- src/Brite/CLI.hs
+++ src/Brite/CLI.hs
@@ -1,5 +1,8 @@
 module Brite.CLI (main) where
 
+import Brite.Dev
+import Brite.Exception
+import Control.Exception (try)
 import System.Environment
 import System.Exit
 import System.IO (stdout)
@@ -13,21 +16,41 @@ main = do
     Right command -> execute command
     Left arg -> do
       displayDoc $
-        bold (red (text "Error:")) <> text " " <>
-        text "Unrecognized argument \"" <>
-        bold (text arg) <> text "\". See below for correct usage." <>
-        hardline <> hardline <> help
+        errorMessage
+          (text "Unrecognized argument \"" <>
+           bold (text arg) <>
+           text "\". See below for the correct usage.") <>
+        hardline <>
+        helpMessage
       return (ExitFailure 1)
 
 -- Displays a pretty print ANSI document.
 displayDoc :: Doc -> IO ()
 displayDoc x = displayIO stdout (renderPretty 0.4 80 x)
 
+{--------------------------------------------------------------------------------------------------}
+{- Commands                                                                                       -}
+{--------------------------------------------------------------------------------------------------}
+
 -- A Brite command to be executed by the CLI.
 data Command
+  -- An empty command means the user did not specify any command line arguments. Here we print out
+  -- the help message and return a failure exit code.
   = EmptyCommand
+
+  -- If the user ran the help command then they are looking to see the help message. Here we print
+  -- out the help message and exit successfully.
+  | HelpCommand
+
+  -- If the user ran the “new” command then they want to create a new Brite project in the
+  -- provided directory.
   | NewCommand
+
+  -- If the user ran the “build” command they want to build the provided file paths.
   | BuildCommand [String]
+
+  -- If the user ran the “reset” command then we delete their cache and any generated resources.
+  | ResetCommand
 
 -- Executes a command and returns an exit code.
 execute :: Command -> IO ExitCode
@@ -35,17 +58,39 @@ execute :: Command -> IO ExitCode
 -- An empty command will print out the help text and exit with a failure. We exit with a failure to
 -- inform other CLI tools that this is not a correct usage of the Brite CLI.
 execute EmptyCommand = do
-  displayDoc help
+  displayDoc helpMessage
   return (ExitFailure 1)
+
+-- Print the help message and exit successfully.
+execute HelpCommand = do
+  displayDoc helpMessage
+  return ExitSuccess
 
 -- TODO: Actually implement the `new` command...
 execute NewCommand = do
-  displayDoc (bold (red (text "Error:")) <> text " The \"new\" command is currently unimplemented." <> hardline)
+  displayDoc (errorMessage (text "The \"new\" command is currently unimplemented."))
   return (ExitFailure 1)
 
 -- TODO: Actually implement the `build` command...
-execute (BuildCommand paths) = do
-  displayDoc (bold (red (text "Error:")) <> text " The \"build\" command is currently unimplemented." <> hardline)
+execute (BuildCommand paths) =
+  if null paths then do
+    displayDoc (errorMessage (text "The \"build\" command is currently unimplemented."))
+    return (ExitFailure 1)
+  else do
+    displayDoc (errorMessage (text "The \"build\" command is currently unimplemented."))
+    return (ExitFailure 1)
+
+-- TODO: Actually implement the `reset` command...
+--
+-- NOTE: We use the name `reset` instead of the traditional `clean` to imply this is a hard reset of
+-- the programmer’s project. We’d prefer that the programmer does not run `reset` if possible since
+-- that will delete their cache slowing down future builds. We’d prefer the user run `brite build`
+-- to make sure all their files are rebuilt. `clean` implies that user is deleting generated
+-- resources so re-generating those resources should not take more time, but in fact the resources
+-- we delete when the programmer calls `brite reset` significantly improves the performance of
+-- their builds.
+execute ResetCommand = do
+  displayDoc (errorMessage (text "The \"reset\" command is currently unimplemented."))
   return (ExitFailure 1)
 
 -- Parses a list of CLI arguments and returns either a command or an error. An error could be an
@@ -53,7 +98,11 @@ execute (BuildCommand paths) = do
 parseArgs :: [String] -> Either String Command
 parseArgs = loop EmptyCommand
   where
-    -- Parse the new command when we see the text `new`.
+    -- If we see common help flags then the user is asking for the help command.
+    loop EmptyCommand ("-h" : args) = loop HelpCommand args
+    loop EmptyCommand ("--help" : args) = loop HelpCommand args
+
+    -- Parse the “new” command.
     loop EmptyCommand ("new" : args) = loop NewCommand args
 
     -- Parse a build command when we see the text `build`. Add every argument to our list of paths
@@ -63,16 +112,28 @@ parseArgs = loop EmptyCommand
     loop (BuildCommand paths) (arg : args) | take 1 arg /= "-" = loop (BuildCommand (arg : paths)) args
     loop (BuildCommand paths) [] = Right (BuildCommand (reverse paths))
 
+    -- Parse the “reset” command.
+    loop EmptyCommand ("reset" : args) = loop ResetCommand args
+
     -- If no one handled this argument then we have an unexpected argument.
     loop _ (arg : _) = Left arg
 
     -- We successfully parsed a command! Hooray! Return it.
     loop command [] = Right command
 
+{--------------------------------------------------------------------------------------------------}
+{- Messages                                                                                       -}
+{--------------------------------------------------------------------------------------------------}
+
+-- An operational error message logged by the CLI.
+errorMessage :: Doc -> Doc
+errorMessage x =
+  bold (red (text "Error:")) <> text " " <> x <> hardline
+
 -- The help text for Brite. Prints a nice little box which is reminiscent of a postcard. Also allows
 -- us to do clever work with alignment since we clearly have a left-hand-side.
-help :: Doc
-help =
+helpMessage :: Doc
+helpMessage =
   black (text "┌" <> text (replicate 78 '─') <> text "┐") <> hardline <>
   boxContent <>
   black (text "└" <> text (replicate 78 '─') <> text "┘") <> hardline
@@ -89,8 +150,17 @@ help =
         , Just $ bold (text "Usage:")
         ] ++
         (map
-          (\(a, b) -> Just $ black (text "$") <> text " " <> fill 32 (text a) <> black (text "# " <> text b))
-          [ ("brite new {name}", "Create a new Brite project.")
-          , ("brite build", "Build the code in your project.")
-          , ("brite build {path...}", "Build the code at these paths.")
+          (\(a, b) -> Just $
+            black (text "$") <>
+            text " " <>
+            fill 32 (text (if isDev then "brite-dev" else "brite") <> text " " <> text a) <>
+            black (text "# " <> text b))
+          [ ("new {name}", "Create a new Brite project.")
+          , ("build", "Build the code in your project.")
+          , ("build {path...}", "Build the code at these paths.")
+
+          -- NOTE: We intentionally don’t document `brite reset` here. We don’t want programmers
+          -- using `brite reset` except in dire circumstances where they absolutely need to reset
+          -- their project since `brite reset` throws away their cache which slows down everything.
+          -- We would prefer the user to run `brite build`.
           ])
