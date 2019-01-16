2bb19cad551c1ae4ace08383e63e126e50fd4bc3
Caleb Meredith <calebmeredith8@gmail.com>
2019-01-16 11:32:15 -0800

    Add the Brite help screen.

--- app/Main.hs
+++ app/Main.hs
@@ -4,26 +4,3 @@ import Brite.CLI
 
 main :: IO ()
 main = cli
-
--- import Brite.Diagnostics
--- import Brite.Syntax.Parser
--- import qualified Brite.Syntax.PrinterAST as PrinterAST
--- import Brite.Syntax.Printer
--- import Brite.Syntax.Tokens
--- import Data.Foldable (traverse_)
--- import Data.Text (Text)
--- import qualified Data.Text.IO as Text.IO
--- import qualified Data.Text.Lazy as Text.Lazy
--- import qualified Data.Text.Lazy.Builder as Text (Builder)
--- import qualified Data.Text.Lazy.Builder as Text.Builder
-
--- main :: IO ()
--- main = do
---   input <- Text.IO.readFile ".play/src/Test.ite"
---   let (mod, diagnostics) = runDiagnosticWriter (parseModule (tokenize input))
---   traverse_ (Text.IO.putStrLn . toStrictText . debugDiagnostic) diagnostics
---   let output = toStrictText (printModule (PrinterAST.convertModule mod))
---   seq output $ Text.IO.writeFile ".play/src/Test.ite" output
-
--- toStrictText :: Text.Builder -> Text
--- toStrictText = Text.Lazy.toStrict . Text.Builder.toLazyText
--- src/Brite/CLI.hs
+++ src/Brite/CLI.hs
@@ -1,4 +1,33 @@
 module Brite.CLI (cli) where
 
+import System.IO (stdout)
+import Text.PrettyPrint.ANSI.Leijen
+
 cli :: IO ()
-cli = putStrLn "Hello, world!"
+cli = do
+  displayIO stdout (renderPretty 0.4 80 help)
+  return ()
+
+help :: Doc
+help =
+  black (text "┌" <> text (replicate 78 '─') <> text "┐") <> hardline <>
+  boxContent <>
+  black (text "└" <> text (replicate 78 '─') <> text "┘") <> hardline
+  where
+    boxContent = mconcat $
+      map
+        (\a ->
+          case a of
+            Nothing -> black (text "│") <> fill 78 mempty <> black (text "│") <> hardline
+            Just b -> black (text "│") <> fill 78 (text " " <> b) <> black (text "│") <> hardline) $
+        [ Just $ bold (text "Brite")
+        , Just $ text "A tool for product development."
+        , Nothing
+        , Just $ bold (text "Usage:")
+        ] ++
+        (map
+          (\(a, b) -> Just $ black (text "$") <> text " " <> fill 32 (text a) <> black (text "# " <> text b))
+          [ ("brite new {name}", "Create a new Brite project.")
+          , ("brite build", "Build the code in your project.")
+          , ("brite build {path...}", "Build the code at these paths.")
+          ])
