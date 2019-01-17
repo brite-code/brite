54d673a75ec3bf19922cd97f252385560887bb98
Caleb Meredith <calebmeredith8@gmail.com>
2019-01-16 20:55:20 -0800

    Add release flag.

--- src/Brite/Dev.hs
+++ src/Brite/Dev.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE CPP #-}
+
 module Brite.Dev (isDev) where
 
 -- Is this build in development mode?
 --
 -- This will be false if we are building a Brite release.
---
--- TODO: Actually make this false when we are building a Brite release.
 isDev :: Bool
+#ifdef RELEASE
+isDev = False
+#else
 isDev = True
+#endif
