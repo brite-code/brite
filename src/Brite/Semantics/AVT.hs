be8a4e5093cb2c257a3e1bfea44f577d16aa020b
Caleb Meredith <calebmeredith8@gmail.com>
2019-01-21 16:55:21 -0800

    Add type checking for expressions.

---
+++ src/Brite/Semantics/AVT.hs
@@ -0,0 +1,25 @@
+-- The Abstract Value Tree (AVT) is a type-checked version of the AST. We use the AVT for
+-- compilation and for serving IDE requests like hover-types.
+
+module Brite.Semantics.AVT
+  ( Constant(..)
+  , Expression(..)
+  , ExpressionNode(..)
+  ) where
+
+import Brite.Semantics.AST (Constant(..))
+import Brite.Semantics.Type
+import Brite.Syntax.Tokens (Range(..))
+
+data Expression = Expression
+  -- The range of source code covered by this expression.
+  { expressionRange :: Range
+  -- The type of this expression.
+  , expressionType :: Polytype
+  -- The representation of this expression.
+  , expressionNode :: ExpressionNode
+  }
+
+data ExpressionNode
+  -- `C`
+  = ConstantExpression Constant
--- src/Brite/Semantics/Check.hs
+++ src/Brite/Semantics/Check.hs
@@ -5,7 +5,42 @@
 module Brite.Semantics.Check () where
 
 import qualified Brite.Semantics.AST as AST
+import Brite.Semantics.AVT
 import Brite.Semantics.Type
+
+-- Type checks an expression AST and returns a typed AVT expression.
+--
+-- This algorithm corresponds to the type inference algorithm in the [MLF thesis][1] described in
+-- section 7.1.
+--
+-- In theory:
+--
+-- > A type inference problem is a triple `(Q, Γ, a)`, where all free type variables in `Γ` are
+-- > bound in `Q`. A pair `(Q', o)` is a solution to this problem if `Q ⊑ Q'` and
+-- > `(Q') Γ ⊦ a : o` holds.
+--
+-- In practice:
+--
+-- * `Q` corresponds to the `Prefix` argument.
+-- * `Γ` corresponds to the `Context` argument.
+-- * `a` corresponds to the `AST.Expression` argument.
+-- * `Q'` corresponds to the `Prefix` argument. We mutate the prefix reference during type checking.
+--   Any future uses of the prefix will be `Q'`. This works because the MLF thesis never asks us to
+--   clone a prefix.
+-- * `o` corresponds to the result of `expressionType` on the returned expression. Not only do we
+--   type check, but we also build an AVT which is semantically equivalent to our input AST.
+--
+-- [1]: https://pastel.archives-ouvertes.fr/file/index/docid/47191/filename/tel-00007132.pdf *)
+checkExpression :: AST.Expression -> Expression
+checkExpression astExpression = case AST.expressionNode astExpression of
+  AST.ConstantExpression constant ->
+    Expression range (checkConstant constant) (ConstantExpression constant)
+
+  where
+    range = AST.expressionRange astExpression
+
+checkConstant :: AST.Constant -> Polytype
+checkConstant (BooleanConstant _) = boolean
 
 -- Checks a type and turns it into a polytype which might possibly have errors.
 checkType :: AST.Type -> Polytype
