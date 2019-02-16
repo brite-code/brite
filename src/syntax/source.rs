025657f6184b7d7929fd9fe84fb13f191cabc696
Caleb Meredith <calebmeredith8@gmail.com>
2019-02-15 18:22:41 -0800

    Add range to token.

--- src/syntax/source.rs
+++ src/syntax/source.rs
@@ -164,6 +164,7 @@ impl Range {
 ///   line_. If a token is trailed by a line comment then that is part of the trailing trivia along
 ///   with the newline which immediately follows but nothing else!
 pub struct Token<'src> {
+    range: Range,
     leading_trivia: Vec<Trivia<'src>>,
     trailing_trivia: Vec<Trivia<'src>>,
     kind: TokenKind<'src>,
