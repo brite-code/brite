b622d924d2bf2a7c06f75864618c8d23ffce5b78
Caleb Meredith <calebmeredith8@gmail.com>
2019-02-21 11:10:07 -0800

    Start writing checker module.

---
+++ src/semantics/checker.rs
@@ -0,0 +1,12 @@
+// use crate::syntax::ast;
+// use std::collections::HashMap;
+
+// fn check_module(module: &ast::Module) {
+//     // let mut context = HashMap::new();
+
+//     for declaration in &module.declarations {}
+// }
+
+// enum Prototype {}
+
+// enum DeclarationPrototype {}
