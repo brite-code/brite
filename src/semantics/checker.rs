0e6e6cb440269c523086072f7cdaa03353a5e0b5
Caleb Meredith <calebmeredith8@gmail.com>
2019-02-21 16:21:12 -0800

    Remove some of the more drastic refactors.

--- src/semantics/checker.rs
+++ src/semantics/checker.rs
@@ -1,9 +1,7 @@
 use crate::diagnostics::{Diagnostic, DiagnosticRef, DiagnosticsCollection};
 use crate::syntax::ast;
 use crate::syntax::{Identifier, Range};
-use std::cell::RefCell;
 use std::collections::HashMap;
-use std::rc::Rc;
 
 /// Checks the Brite Abstract Syntax Tree (AST) for errors and warnings. Reports diagnostics for any
 /// invalid code.
@@ -16,31 +14,31 @@ use std::rc::Rc;
 /// It follows that what code is invalid depends a lot on the language runtime. If the language
 /// runtime imposes certain constraints on code, then our checker should make sure those constraints
 /// are indeed maintained.
-pub struct Checker<'errs, 'ast> {
+pub struct Checker<'errs> {
     /// The collection we report diagnostics to. Please use `Checker::report_diagnostic` instead of
     /// accessing our collection directly.
     _diagnostics: &'errs mut DiagnosticsCollection,
     /// All the identifiers currently in scope and the behavior of each identifier in a program.
-    scope: HashMap<Identifier, ScopeEntry<'ast>>,
+    scope: HashMap<Identifier, ScopeEntry>,
 }
 
 /// The behavior of an identifier in scope.
-struct ScopeEntry<'ast> {
+struct ScopeEntry {
     /// The range of the scope entry’s name.
     range: Range,
     /// The kind of scope entry this is.
-    kind: ScopeEntryKind<'ast>,
+    kind: ScopeEntryKind,
 }
 
-enum ScopeEntryKind<'ast> {
-    Declaration(Rc<RefCell<CheckerDeclaration<'ast>>>),
+/// The kind of a [`ScopeEntry`].
+enum ScopeEntryKind {
+    /// The entry is a function declaration.
+    FunctionDeclaration,
+    /// The entry is a class declaration.
+    ClassDeclaration { base: bool },
 }
 
-enum CheckerDeclaration<'ast> {
-    Unchecked(&'ast ast::Declaration),
-}
-
-impl<'errs, 'ast> Checker<'errs, 'ast> {
+impl<'errs> Checker<'errs> {
     /// Creates a new type checker context.
     pub fn new(diagnostics: &'errs mut DiagnosticsCollection) -> Self {
         Checker {
@@ -49,46 +47,40 @@ impl<'errs, 'ast> Checker<'errs, 'ast> {
         }
     }
 
-    /// Checks an AST module for semantic errors.
-    pub fn check_module(&mut self, module: &'ast ast::Module) {
-        // A module is made up of many declarations. Type checking declarations is a bit tricky
-        // since they’re mutually recursive. (A declaration may reference another declaration
-        // ordered _after_ itself in source code.) Here’s how we type check declarations:
-        //
-        // 1. Add all our declarations to our context in an “unchecked” state.
-        // 2. Go through our unchecked declarations list and check them.
-        // 3. If we reference a
-        let declarations = module
-            .declarations
-            .iter()
-            .map(|declaration| {
-                // Get the name of our declaration.
-                let name = match declaration {
-                    ast::Declaration::Function(function) => &function.name,
-                    ast::Declaration::Class(class) => &class.name,
-                };
-                //
-                let declaration = Rc::new(RefCell::new(CheckerDeclaration::Unchecked(declaration)));
-
-                // If we’ve already seen this name then report an error. We’ll still type-check the
-                // declaration, but any references will get access to the first declaration we saw.
-                if let Some(entry) = self.scope.get(&name.identifier) {
-                    self.report_diagnostic(Diagnostic::declaration_name_already_used(
-                        name.range,
-                        name.identifier,
-                        entry.range,
-                    ));
-                } else {
-                    self.scope.insert(
-                        name.identifier,
-                        ScopeEntry {
-                            range: name.range,
-                            kind: ScopeEntryKind::Declaration(Rc::clone(&declaration)),
-                        },
-                    );
+    /// Checks an AST module for errors.
+    pub fn check_module(&mut self, module: &ast::Module) {
+        // Add all our declarations to scope. We need to do this before type checking our
+        // declarations because all declarations are mutually recursive.
+        for declaration in &module.declarations {
+            // Get the name and the scope entry kind of our declaration.
+            let (name, entry_kind) = match declaration {
+                ast::Declaration::Function(function) => {
+                    (&function.name, ScopeEntryKind::FunctionDeclaration)
                 }
-                declaration
-            }).collect();
+                ast::Declaration::Class(class) => (
+                    &class.name,
+                    ScopeEntryKind::ClassDeclaration { base: class.base },
+                ),
+            };
+            // If we’ve already seen this declaration name then report an error. We’ll still
+            // type-check the declaration, but any references will get access to the first
+            // declaration we saw.
+            if let Some(entry) = self.scope.get(&name.identifier) {
+                self.report_diagnostic(Diagnostic::declaration_name_already_used(
+                    name.range,
+                    name.identifier,
+                    entry.range,
+                ));
+            } else {
+                self.scope.insert(
+                    name.identifier,
+                    ScopeEntry {
+                        range: name.range,
+                        kind: entry_kind,
+                    },
+                );
+            }
+        }
     }
 
     /// Reports a diagnostic.
