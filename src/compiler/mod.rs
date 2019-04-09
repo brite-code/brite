14e401744b3b10286df70fe6315d1d8f09af15ef
Caleb Meredith <calebmeredith8@gmail.com>
2019-04-08 23:08:10 -0700

    Write down optimizations I want to implement.

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,1 +1,8 @@
+//! TODO: Optimizations
+//!
+//! - `@inline`: Inlines a function everywhere it is called. If used on a class then we inline the
+//!   class everywhere it is used.
+//! - `@outline`: Opposite of inlining. Takes the function implementation and puts it into a new
+//!   code chunk. The function must return a promise to represent the asynchronous fetch.
+
 pub mod js;
