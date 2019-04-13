b9c9ff2a88cc281cf1a10d90e33a25ec5cf60261
Caleb Meredith <calebmeredith8@gmail.com>
2019-04-13 12:38:34 -0700

    Refactor type checker a bit.

--- src/checker/checker2.rs
+++ src/checker/checker2.rs
@@ -17,25 +17,21 @@ pub fn precheck_module(diagnostics: &mut DiagnosticsCollection, module: &Module)
     for declaration in &module.declarations {
         let declaration_name = declaration.name();
 
-        let recheck_declaration = if let Some((other_declaration_range, _)) =
-            declarations.get(&declaration_name.identifier)
-        {
-            diagnostics.report(Diagnostic::declaration_name_already_used(
-                declaration_name.range,
-                declaration_name.identifier.clone(),
-                other_declaration_range.clone(),
-            ));
-            Err(declaration)
-        } else {
-            declarations.insert(
-                declaration_name.identifier.clone(),
-                (
+        let recheck_declaration =
+            if let Some(other_declaration) = declarations.get(&declaration_name.identifier) {
+                diagnostics.report(Diagnostic::declaration_name_already_used(
                     declaration_name.range,
-                    PrecheckDeclarationLazy::Unchecked(declaration),
-                ),
-            );
-            Ok(declaration_name)
-        };
+                    declaration_name.identifier.clone(),
+                    other_declaration.name_range(),
+                ));
+                Err(declaration)
+            } else {
+                declarations.insert(
+                    declaration_name.identifier.clone(),
+                    PrecheckDeclarationLazy::unchecked(declaration_name.range, declaration),
+                );
+                Ok(declaration_name)
+            };
 
         recheck_declarations.push(recheck_declaration);
     }
@@ -65,8 +61,7 @@ pub fn precheck_module(diagnostics: &mut DiagnosticsCollection, module: &Module)
     }
 }
 
-type PrecheckDeclarationMap<'source> =
-    HashMap<Identifier, (Range, PrecheckDeclarationLazy<'source>)>;
+type PrecheckDeclarationMap<'source> = HashMap<Identifier, PrecheckDeclarationLazy<'source>>;
 
 enum PrecheckDeclaration {
     Function(PrecheckFunctionDeclaration),
@@ -91,10 +86,11 @@ fn precheck_declaration(
 }
 
 struct PrecheckFunctionDeclaration {
+    name_range: Range,
     // name: Identifier,
-// parameters: Vec<(Pattern, Type)>,
-// return_type: Type,
-// body: Block,
+    // parameters: Vec<(Pattern, Type)>,
+    // return_type: Type,
+    // body: Block,
 }
 
 fn precheck_function_declaration(
@@ -103,10 +99,13 @@ fn precheck_function_declaration(
     declaration: &FunctionDeclaration,
 ) -> PrecheckFunctionDeclaration {
     // unimplemented!()
-    PrecheckFunctionDeclaration {}
+    PrecheckFunctionDeclaration {
+        name_range: declaration.name.range,
+    }
 }
 
 struct PrecheckClassDeclaration {
+    name_range: Range,
     // name: Identifier,
 }
 
@@ -116,19 +115,54 @@ fn precheck_class_declaration(
     declaration: &ClassDeclaration,
 ) -> PrecheckClassDeclaration {
     // unimplemented!()
-    PrecheckClassDeclaration {}
+    PrecheckClassDeclaration {
+        name_range: declaration.name.range,
+    }
 }
 
+/// To type check a module, we first look at all the declarations in our module and add “unchecked“
+/// references to the declaration. We then go through each declaration and type check it. If one
+/// declaration depends on another (like `class extends`) we will make sure to check that class even
+/// if it appears after our own in source code.
 enum PrecheckDeclarationLazy<'source> {
+    /// The declaration is unchecked or is currently in the process of type checking.
+    Unchecked {
+        /// The range of the declaration’s name.
+        name_range: Range,
+        /// The declaration which needs to be checked. If `None` then we are currently type checking
+        /// this declaration.
+        declaration: Option<&'source Declaration>,
+        /// References to the declaration that we find before we finish checking.
+        references: Vec<()>,
+    },
+    /// We’ve finished checking the declaration.
     Checked(PrecheckDeclaration),
-    Checking,
-    Unchecked(&'source Declaration),
 }
 
 impl<'source> PrecheckDeclarationLazy<'source> {
+    fn unchecked(name_range: Range, declaration: &'source Declaration) -> Self {
+        PrecheckDeclarationLazy::Unchecked {
+            name_range,
+            declaration: Some(declaration),
+            references: Vec::new(),
+        }
+    }
+
+    fn name_range(&self) -> Range {
+        match self {
+            PrecheckDeclarationLazy::Unchecked { name_range, .. } => *name_range,
+            PrecheckDeclarationLazy::Checked(PrecheckDeclaration::Function(declaration)) => {
+                declaration.name_range
+            }
+            PrecheckDeclarationLazy::Checked(PrecheckDeclaration::Class(declaration)) => {
+                declaration.name_range
+            }
+        }
+    }
+
     fn is_checking(&self) -> bool {
         match self {
-            PrecheckDeclarationLazy::Checking => true,
+            PrecheckDeclarationLazy::Unchecked { declaration, .. } => declaration.is_none(),
             _ => false,
         }
     }
@@ -143,33 +177,46 @@ fn resolve_precheck_declaration<'scope>(
     range: &Range,
     identifier: &Identifier,
 ) -> Result<&'scope PrecheckDeclaration, DiagnosticRef> {
-    if let Some((declaration_range, declaration_lazy)) = declarations.get_mut(identifier) {
+    if let Some(declaration_lazy) = declarations.get_mut(identifier) {
         match declaration_lazy {
+            // If we’ve already checked this declaration then do nothing...
             PrecheckDeclarationLazy::Checked(_) => {}
-            PrecheckDeclarationLazy::Checking => {
-                return Err(diagnostics.report(Diagnostic::declaration_cycle_detected(
-                    range.clone(),
-                    identifier.clone(),
-                    declaration_range.clone(),
-                )));
-            }
-            PrecheckDeclarationLazy::Unchecked(declaration) => {
-                let declaration: &Declaration = *declaration;
-                *declaration_lazy = PrecheckDeclarationLazy::Checking;
-                let declaration = precheck_declaration(diagnostics, declarations, declaration);
-                let declaration_lazy = &mut declarations.get_mut(identifier).unwrap().1;
-                debug_assert!(declaration_lazy.is_checking());
-                *declaration_lazy = PrecheckDeclarationLazy::Checked(declaration);
+
+            // If we have not yet checked this declaration...
+            PrecheckDeclarationLazy::Unchecked {
+                name_range,
+                declaration,
+                references,
+            } => {
+                // Take the declaration we need to check. This will set the `declaration` field to
+                // `None`. That way we can detect circular references.
+                if let Some(declaration) = declaration.take() {
+                    let declaration = precheck_declaration(diagnostics, declarations, declaration);
+                    let declaration_lazy = declarations.get_mut(identifier).unwrap();
+                    debug_assert!(declaration_lazy.is_checking());
+                    *declaration_lazy = PrecheckDeclarationLazy::Checked(declaration);
+                } else {
+                    // If we are already checking this declaration then we found a
+                    // circular reference!
+                    return Err(diagnostics.report(Diagnostic::declaration_cycle_detected(
+                        range.clone(),
+                        identifier.clone(),
+                        name_range.clone(),
+                    )));
+                }
             }
         }
     } else {
+        // Uh oh. We could not find the declaration that was requested.
         return Err(diagnostics.report(Diagnostic::identifier_not_found(
             range.clone(),
             identifier.clone(),
         )));
     }
     match declarations.get(identifier) {
-        Some((_, PrecheckDeclarationLazy::Checked(declaration))) => Ok(declaration),
+        Some(PrecheckDeclarationLazy::Checked(declaration)) => Ok(declaration),
         _ => unreachable!(),
     }
 }
+
+fn check_type(diagnostics: &mut DiagnosticsCollection, type_: &Type) {}
